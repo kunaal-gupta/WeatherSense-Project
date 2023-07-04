41eff68226bfb2d8d0f8d53d3664f92008867f4b
Kunaal Gupta <kunaalgupta@hotmail.com>
2023-07-04 17:30:59 -0600

    added animation to load button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,6 @@
+import Home from "./Welcome";
 import "./App.css"
 <script src="main.js?v=12345"></script>
-
-
-function Welcome() {
-  return (
-    <>
-    <div className="background">
-      <div className="Welcome-section">
-        <div className="welcome-empty-section">
-        </div>
-        <div className="Welcome-heading">
-          <div> Welcome to TempDashboard ReaXpress </div>
-          <button className="Welcome-button"> Load Dashboard</button>
-        </div>
-      </div>
-      </div>
-    </>
-  )
-}
 
 
 export default function App() {
@@ -25,7 +8,7 @@ export default function App() {
   return (
 
     <>
-      <Welcome />
+      <Home />
     </>
 
   );
---
+++ client/src/Welcome.jsx
@@ -0,0 +1,22 @@
+// import { TiWeatherDownpour } from 'react-icons/fa';
+
+export default function Home() {
+    return (
+      <>
+      <div className="background">
+        <div className="Welcome-section">
+          <div className="welcome-empty-section">
+         
+          </div>
+          <div className="Welcome-heading">
+            <div> Welcome to TempDashboard ReaXpress </div>
+            <button className="Welcome-button"> Load Dashboard</button>
+            {/* <i className='fas fa-cloud-moon-rain'></i> */}
+  
+          </div>
+        </div>
+        </div>
+      </>
+    )
+  }
+  
