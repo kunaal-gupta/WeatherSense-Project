b593d5943d1b50759a2df501d798795a71c4a0c6
Kunaal Gupta <kunaalgupta@hotmail.com>
2023-07-04 22:18:46 -0600

    added image & loader

--- client/src/Welcome.jsx
+++ client/src/Welcome.jsx
@@ -1,23 +1,31 @@
 import { WiDaySnowWind } from "react-icons/wi";
 import "./App.css"
+import { DiReact, DiNodejs, DiJavascript1 } from "react-icons/di";
+import mountainImage from './mountain.jpg';
+
 
 export default function Home() {
-    return (
-      <>
+  return (
+
+    <>
       <div className="background">
         <div className="Welcome-section">
           <div className="welcome-empty-section">
-          < WiDaySnowWind className="WeatherIcon"/>
+            < WiDaySnowWind className="WeatherIcon" />
+
           </div>
           <div className="Welcome-heading">
-            <div> Welcome to TempDashboard ReaXpress </div>
+            <div> Welcome to Temp<span style={{ color: 'Orange' }}>Dashboard</span>-Rea<span style={{ color: 'Orange' }}>Xpress</span> </div>
             <button className="Welcome-button"> Load Dashboard</button>
-            {/* <i className='fas fa-cloud-moon-rain'></i> */}
-  
+
+            {/* <div className="built-by" > Made using
+              <div className="icons"><DiReact /> <DiNodejs /> < DiJavascript1 /> </div>
+            </div> */}
+            <div className="loader">.</div>
+
           </div>
         </div>
-        </div>
-      </>
-    )
-  }
-  
+      </div>
+    </>
+  )
+}
