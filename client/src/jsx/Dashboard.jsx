1b830507dfec8cddb892fbc5f589087ed5e9af27
Kunaal Gupta <kunaalgupta@hotmail.com>
2023-07-06 00:09:39 -0600

    added parameters to dashboard ui

--- client/src/jsx/Dashboard.jsx
+++ client/src/jsx/Dashboard.jsx
@@ -11,17 +11,30 @@ function Dashboard() {
 
 					</div>
 					<div className="dash-right-section">
-						<div className='daily-temp'>
-							<div className='temp-boxes'>Sun</div>
-							<div className='temp-boxes'>Mon</div>
-							<div className='temp-boxes'>Tues</div>
-							<div className='temp-boxes'>Wed</div>
-							<div className='temp-boxes'>Thurs</div>
-							<div className='temp-boxes'>Fri</div>
-							<div className='temp-boxes'>Sat</div>
+						<div className='dash-right-content'>
+							<div className='daily-temp'>
+								<div className='temp-boxes'>Sun</div>
+								<div className='temp-boxes'>Mon</div>
+								<div className='temp-boxes'>Tues</div>
+								<div className='temp-boxes'>Wed</div>
+								<div className='temp-boxes'>Thurs</div>
+								<div className='temp-boxes'>Fri</div>
+								<div className='temp-boxes'>Sat</div>
+
+							</div>
+
+							<div className='other-weather-details'>
+							<div className='humidity'>Humidity</div>
+							<div className='humidity'>Wind</div>
+							<div className='humidity'>Feels Like</div>
+							<div className='humidity'>Precipitation</div>
+							<div className='humidity'>UV Index</div>
+							<div className='humidity'>AQI</div>
 
 						</div>
+						</div>
 					</div>
+
 				</div>
 			</div>
 
