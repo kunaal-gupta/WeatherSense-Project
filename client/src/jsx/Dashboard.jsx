8695d7c9c7f52aa9a0532dc7a565e99de3c7c25a
Kunaal Gupta <kunaalgupta@hotmail.com>
2023-07-06 17:30:39 -0600

    redesinged dashboard layout for therm

--- client/src/jsx/Dashboard.jsx
+++ client/src/jsx/Dashboard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import "../css/dashboard.css";
 import WindGraph from './Radialbar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSun,  faMoon, faWind, faSnowflake, faCloud, faRainbow, faTint, faCloudSun } from '@fortawesome/free-solid-svg-icons';
+import { faSun, faMoon, faWind, faSnowflake, faCloud, faRainbow, faTint, faCloudSun } from '@fortawesome/free-solid-svg-icons';
 
 
 function Dashboard() {
@@ -17,44 +17,49 @@ function Dashboard() {
 						<div className='dash-right-content'>
 							<div className='daily-temp'>
 								<div className='temp-boxes'>
-								<div><FontAwesomeIcon icon={faSun} size='xl'  style={{ color: '#ff7b00' }} /></div>
+									<div><FontAwesomeIcon icon={faSun} size='xl' style={{ color: '#ff7b00' }} /></div>
 									<div style={{ marginTop: '10%', fontSize: '80%' }}>Mon</div>
 									<div style={{ marginTop: '1%', fontSize: '80%' }}>12°C</div>
 								</div>
 								<div className='temp-boxes'>
-									<div><FontAwesomeIcon icon={faCloudSun }  size='xl' style={{ color: 'Yellow' }} /></div>
+									<div><FontAwesomeIcon icon={faCloudSun} size='xl' style={{ color: 'Yellow' }} /></div>
 									<div style={{ marginTop: '10%', fontSize: '80%' }}>Tues</div>
 									<div style={{ marginTop: '1%', fontSize: '80%' }}>12°C</div>
 
 
 								</div>							<div className='temp-boxes'>
-									<div><FontAwesomeIcon icon={faTint}  size='xl' style={{ color: 'SkyBlue' }} /></div>
+									<div><FontAwesomeIcon icon={faTint} size='xl' style={{ color: 'SkyBlue' }} /></div>
 									<div style={{ marginTop: '10%', fontSize: '80%' }}>Wed</div>
 									<div style={{ marginTop: '1%', fontSize: '80%' }}>12°C</div>
 
 
 								</div>							<div className='temp-boxes'>
-									<div><FontAwesomeIcon icon={faCloud}  size='xl' style={{ color: 'Gray' }} /></div>
+									<div><FontAwesomeIcon icon={faCloud} size='xl' style={{ color: 'Gray' }} /></div>
 									<div style={{ marginTop: '10%', fontSize: '80%' }}>Thur</div>
 									<div style={{ marginTop: '1%', fontSize: '80%' }}>12°C</div>
 
 
 								</div>														<div className='temp-boxes'>
-									<div><FontAwesomeIcon icon={faSnowflake}  size='xl' style={{ color: 'white' }} /></div>
+									<div><FontAwesomeIcon icon={faSnowflake} size='xl' style={{ color: 'white' }} /></div>
 									<div style={{ marginTop: '10%', fontSize: '80%' }}>Fri</div>
 									<div style={{ marginTop: '1%', fontSize: '80%' }}>12°C</div>
 
 
 								</div>							<div className='temp-boxes'>
-									<div><FontAwesomeIcon icon={faWind}  size='xl'  style={{ color: '#ff7b00' }} /></div>
-									<div style={{ marginTop: '10%', fontSize: '80%'}}>Sat</div>
+									<div><FontAwesomeIcon icon={faWind} size='xl' style={{ color: '#ff7b00' }} /></div>
+									<div style={{ marginTop: '10%', fontSize: '80%' }}>Sat</div>
 									<div style={{ marginTop: '1%', fontSize: '80%' }}>12°C</div>
 
 
 								</div>
 
 							</div>
-							<WindGraph />
+							<div className='dash-right-bottom-section'>
+								<div className='thermom'></div>
+								<div className='Wind'><WindGraph /></div>
+								<div className='thermom'></div>
+							</div>
+
 
 						</div>
 					</div>
