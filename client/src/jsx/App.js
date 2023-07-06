282da748d0537fed9ef06b5390ecaa2945623d56
Kunaal Gupta <kunaalgupta@hotmail.com>
2023-07-05 19:14:41 -0600

    set up Urls

--- client/src/jsx/App.js
+++ client/src/jsx/App.js
@@ -4,18 +4,23 @@ import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 import Home from "./Home";
 import Contact from "./Contact";
 import About from "./About";
+import Dashboard from "./Dashboard.jsx";
 
 export default function App() {
   return (
     <BrowserRouter>
       <nav>
         <Link to="/">Home</Link>
+        <Link to="/dashboard">Dashboard</Link>
         <Link to="/about">About</Link>
         <Link to="/contact">Contact</Link>
       </nav>
+
+        <Route exact path="/dashboard" component={Dashboard} />
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
         <Route path="/contact" component={Contact} />
+
     </BrowserRouter>
   );
 }
--- client/src/jsx/Home.js
+++ client/src/jsx/Home.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import { WiDaySnowWind } from "react-icons/wi";
+import { Link } from "react-router-dom";
 
-function Home (){
-	return <h1>Welcome to the world of Geeks!</h1>
+import "../css/App.css"
+
+
+export default function Home() {
+  return (
+
+    <>
+      <div className="background">
+        <div className="Welcome-section">
+          <div className="welcome-empty-section">
+            < WiDaySnowWind className="WeatherIcon" />
+
+          </div>
+          <div className="Welcome-heading">
+            <div> Welcome to Weather<span style={{ color: 'White' }}>Sense</span></div>
+            <Link to="/dashboard">
+              <button className="Welcome-button">Load Dashboard</button>
+            </Link>           
+             <div className="loader"></div>
+
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
-
-export default Home;
