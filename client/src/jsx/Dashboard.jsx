d9f4b4fe23ea893f5df1d119389139d789e0adbf
Kunaal Gupta <kunaalgupta@hotmail.com>
2023-07-05 23:31:47 -0600

    temperature design in dashboard

--- client/src/jsx/Dashboard.jsx
+++ client/src/jsx/Dashboard.jsx
@@ -7,11 +7,20 @@ function Dashboard() {
 		<>
 			<div className="dash-background">
 				<div className="dash-section">
-					<div className="dash-empty-section">
+					<div className="dash-left-section">
 
 					</div>
-					<div className="dash-heading">
-			
+					<div className="dash-right-section">
+						<div className='daily-temp'>
+							<div className='temp-boxes'>Sun</div>
+							<div className='temp-boxes'>Mon</div>
+							<div className='temp-boxes'>Tues</div>
+							<div className='temp-boxes'>Wed</div>
+							<div className='temp-boxes'>Thurs</div>
+							<div className='temp-boxes'>Fri</div>
+							<div className='temp-boxes'>Sat</div>
+
+						</div>
 					</div>
 				</div>
 			</div>
