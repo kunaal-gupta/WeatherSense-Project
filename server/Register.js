05a8a7af113f07fc7cf0411659d261a93309d920
Kunaal Gupta <kunaalgupta@hotmail.com>
2023-07-07 23:35:09 -0600

    defined registerCred function

---
+++ server/Register.js
@@ -0,0 +1,31 @@
+const { MongoClient } = require("mongodb");
+
+const url = "mongodb+srv://kunaalgupta:kDGpvJLZDibg7PbR@clusterinit.ss7ajbc.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(url);
+
+async function RegisterCred(Username, Password, Name, City) {
+    try {
+        await client.connect();
+
+        const db = client.db('WeatherSenseDB');
+        const col = db.collection('LoginAuthentication');
+
+        const query = { 'Username': Username, 'Password': Password, 'Name': Name, 'City': City};
+        const result = await col.insertOne(query)
+
+        if (result) {
+            return 1;
+        } else {
+            return 0
+        }
+    }
+    finally {
+        await client.close();
+    }
+    
+}
+
+
+module.exports = { RegisterCred };
+
+
