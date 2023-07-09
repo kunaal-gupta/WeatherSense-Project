605d1d953427aeddd02242de40d869a9429871af
Kunaal Gupta <kunaalgupta@hotmail.com>
2023-07-08 23:09:48 -0600

    filtered API data on server

--- server/crud.js
+++ server/crud.js
@@ -15,7 +15,37 @@ async function FetchAPIdata() {
 
   try {
     const response = await axios.request(options);
-    return response.data;
+    console.log(response.data)
+
+    const uv = response.data.current.uv;
+    const tempC = response.data.current.temp_c;
+    const feelsLikeC = response.data.current.feelslike_c;
+    const windMph = response.data.current.wind_mph;
+    const pressureMb = response.data.current.pressure_mb;
+    const visibilityKm = response.data.current.vis_km;
+    const gustMph = response.data.current.gust_mph;
+    const location = respones.data.location.name + ', ' + respones.data.location.Country + ', ' +  respones.data.location.region 
+    const longitude = response.data.current.lat;
+    const latitude = response.data.current.lon;
+    const WindDeg  = response.data.wind_mph;
+    const WindDir =  response.data.WindDir;
+    const Precipitation = response.data.Precipitation
+
+    data.UV = uv;
+    data.Temp = tempC;
+    data.FeelsLike = feelsLikeC;
+    data.Wind = windMph;
+    data.Pressure = pressureMb;
+    data.Visibility = visibilityKm;
+    data.Gust = gustMph;    
+    data.location = location;
+    data.longitude = longitude;
+    data.latitude = latitude;
+    data.windDeg = WindDeg;
+    data.WindDir = WindDir;
+    data.Precipitation = Precipitation;
+
+    return data
   } catch (error) {
     console.error(error);
   }
