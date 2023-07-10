5e8aa22e3eb16655b2732543a2d980ab42a9002d
Kunaal Gupta <kunaalgupta@hotmail.com>
2023-07-09 22:26:11 -0600

    added about us section

--- client/src/jsx/About.jsx
+++ client/src/jsx/About.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 
-function About () {
+function About() {
 	return <div>
-		<h2>GeeksforGeeks is a computer science portal for geeks!</h2>
+		<center><h1  style={{fontSize: '3.5vw'}}>Weather Sense</h1></center>
 
-		Read more about us at :
-		<a href="https://www.geeksforgeeks.org/about/">
-			https://www.geeksforgeeks.org/about/
+		<a style={{fontSize: '2vw'}}><center> 			
+			This is a full-stack project developed using MERN Stack (MongoDB, ExpressJS, React & Nodejs) <br></br>
+			that fetches Weather API to display weather parameters like Humidity, UV index etc at frontend. 
+			
+			<br></br><br></br>MongoDB database at the backend stores user details like Name, Email, Password & 
+			<br></br>City to display user specific details <br></br>
+
+			<br></br>ExpressJS & NodeJs at the backend connects React framework with database and makes <br></br>  API requests for fetching data
+		</center>
+
 		</a>
 	</div>
 }
--- client/src/jsx/Contact.jsx
+++ client/src/jsx/Contact.jsx
@@ -2,10 +2,7 @@ import React from 'react';
 
 function Contact (){
 return <address>
-			You can find us here:<br />
-			GeeksforGeeks<br />
-			5th & 6th Floor, Royal Kapsons, A- 118, <br />
-			Sector- 136, Noida, Uttar Pradesh (201305)
+		Please connect with me at <a href='mailto:kunaalgupta@hotmail.com'>kunaalgupta@hotmail.com</a> 
 		</address>
 }
 
